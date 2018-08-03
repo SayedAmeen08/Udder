bdefee003dcbb6e25f7be3908ae803d54b54b722
Ameen Al-Habbobi <Ameen@ameens-mac-mini.local>
2018-08-03 17:55:27 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ameen on 2018-08-03.
+//  Copyright © 2018 Ameen Al-Habbobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
